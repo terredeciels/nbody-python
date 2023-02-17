5d31b8668d0b2834c99954a9d323ba46cb352776
gilles <chekunive@gmail.com>
2023-02-17 15:43:09 +0100

    code sans l'énergie

--- nbody.py
+++ nbody.py
@@ -1,24 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-"""
-Create Your Own N-body Simulation (With Python)
-Philip Mocz (2020) Princeton Univeristy, @PMocz
-
-Simulate orbits of stars interacting due to gravity
-Code calculates pairwise forces according to Newton's Law of Gravity
-"""
-
 
 def getAcc(pos, mass, G, softening):
-    """
-    Calculate the acceleration on each particle due to Newton's Law 
-	pos  is an N x 3 matrix of positions
-	mass is an N x 1 vector of masses
-	G is Newton's Gravitational constant
-	softening is the softening length
-	a is N x 3 matrix of accelerations
-	"""
     # positions r = [x,y,z] for all particles
     x = pos[:, 0:1]
     y = pos[:, 1:2]
@@ -41,41 +25,6 @@ def getAcc(pos, mass, G, softening):
     a = np.hstack((ax, ay, az))
 
     return a
-
-
-def getEnergy(pos, vel, mass, G):
-    """
-	Get kinetic energy (KE) and potential energy (PE) of simulation
-	pos is N x 3 matrix of positions
-	vel is N x 3 matrix of velocities
-	mass is an N x 1 vector of masses
-	G is Newton's Gravitational constant
-	KE is the kinetic energy of the system
-	PE is the potential energy of the system
-	"""
-    # Kinetic Energy:
-    KE = 0.5 * np.sum(np.sum(mass * vel ** 2))
-
-    # Potential Energy:
-
-    # positions r = [x,y,z] for all particles
-    x = pos[:, 0:1]
-    y = pos[:, 1:2]
-    z = pos[:, 2:3]
-
-    # matrix that stores all pairwise particle separations: r_j - r_i
-    dx = x.T - x
-    dy = y.T - y
-    dz = z.T - z
-
-    # matrix that stores 1/r for all particle pairwise particle separations
-    inv_r = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
-    inv_r[inv_r > 0] = 1.0 / inv_r[inv_r > 0]
-
-    # sum over upper triangle, to count each interaction only once
-    PE = G * np.sum(np.sum(np.triu(-(mass * mass.T) * inv_r, 1)))
-
-    return KE, PE
 
 
 def main():
@@ -104,7 +53,7 @@ def main():
     acc = getAcc(pos, mass, G, softening)
 
     # calculate initial energy of system
-    KE, PE = getEnergy(pos, vel, mass, G)
+    # KE, PE = getEnergy(pos, vel, mass, G)
 
     # number of timesteps
     Nt = int(np.ceil(tEnd / dt))
@@ -113,9 +62,9 @@ def main():
     pos_save = np.zeros((N, 3, Nt + 1))
     pos_save[:, :, 0] = pos
     KE_save = np.zeros(Nt + 1)
-    KE_save[0] = KE
+    # KE_save[0] = KE
     PE_save = np.zeros(Nt + 1)
-    PE_save[0] = PE
+    # PE_save[0] = PE
     t_all = np.arange(Nt + 1) * dt
 
     # prep figure
@@ -142,12 +91,12 @@ def main():
         t += dt
 
         # get energy of system
-        KE, PE = getEnergy(pos, vel, mass, G)
+        #  KE, PE = getEnergy(pos, vel, mass, G)
 
         # save energies, positions for plotting trail
         pos_save[:, :, i + 1] = pos
-        KE_save[i + 1] = KE
-        PE_save[i + 1] = PE
+        #  KE_save[i + 1] = KE
+        #  PE_save[i + 1] = PE
 
         # plot in real time
         if plotRealTime or (i == Nt - 1):
